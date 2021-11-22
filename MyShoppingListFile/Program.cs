648bbe13a00f8f780f1ff33c9ae71889470230dc
Lisanne Nurk <lnurk@tktk.ee>
2021-11-22 10:47:24 +0200

    add new projects

---
+++ MyShoppingListFile/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MyShoppingListFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileLocation = @"C:\Users\lisan\samples\ShoppingList";
+            string fileName = @"\\myshoppinglist.txt";
+
+            string[] arrayFromFile = File.ReadAllLines($"{fileLocation}{fileName}");
+            List<string> myshoppinglist = arrayFromFile.ToList<string>();
+
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Would you like to add an item to shopping list? Y/N:");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (userInput == 'y')
+                {
+                    Console.WriteLine("Enter your wish:");
+                    string userWish = Console.ReadLine();
+                    myshoppinglist.Add(userWish);
+                }
+                else
+                {
+                    loopActive = false;
+                    Console.WriteLine("Take care!");
+                }
+            }
+
+            Console.Clear();
+
+            foreach (string wish in myshoppinglist)
+            {
+                Console.WriteLine($"Your wish: {wish}");
+            }
+
+            File.WriteAllLines($"{fileLocation}{fileName}", myshoppinglist);
+        }
+    }
+}
